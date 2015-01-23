a0fc358bfd287f50418f8e2dd6cc46809eb06515
sidraina89 <sidraina89@gmail.com>
2015-01-22 22:34:28 -0500

    Titanic Data Analysis - 1st commit

---
+++ TitanicAnalysis.R
@@ -0,0 +1,6 @@
+#Importing Training Data Set
+train <- read.csv("C:/Sid/R Workspace/Data-Science-Legato/train.csv")
+View(train)
+#Importing Test Data Set
+test <- read.csv("C:/Sid/R Workspace/Data-Science-Legato/test.csv")
+View(test)
